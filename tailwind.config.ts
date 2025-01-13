f75f2dc0596fd85e1002721ca3469ebd8f2c3778
risik34 <risimn7@gmail.com>
2025-01-13 09:47:36 +0530

    style: add brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary:"#CEEF4C",
+        secondary:"#E7FF8F"
       },
     },
   },
